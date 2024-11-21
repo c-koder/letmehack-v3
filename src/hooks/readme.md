69113f8a080e952d14af6e719827b27f158047c4
sadeeshachiran <pmsckaushalya@gmail.com>
2024-11-22 01:25:41 +0530

    update project version

---
+++ src/hooks/readme.md
@@ -0,0 +1,1 @@
+# Add your hooks in this directory
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,26 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        themePrimary: '#01260D',
+        themePrimaryLighter: '#2AE55C',
+      },
+      boxShadow: {
+        boxShadow: '0px 3px 6px rgba(0, 119, 111, 0.08)',
+        sectionShadow: '0px 6px 24px rgba(6, 129, 121, 0.08)',
+        card: '0px 20px 32px -8px rgba(6, 129, 121, 0.16)',
+        owlCard: '0px 6px 12px -4px rgba(6, 129, 121, 0.1)',
+        searchShadow: '0px 12px 32px -6px rgba(0, 119, 111, 0.2)',
+        filterShadow: '0px 2px 6px rgba(6, 129, 121, 0.08)',
+        frontShadow: ' 0px 24px 24px -12px rgba(6, 129, 121, 0.12)',
+      },
+    },
+    container: {
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1350px',
       },
     },
   },
