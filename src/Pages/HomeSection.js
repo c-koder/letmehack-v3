618b37ae7b4e6ed54116e72ceb01c0e5d27087c7
Sajith Rathna Sampath <sajithsampath721@gmail.com>
2024-11-23 22:37:50 +0530

    register page add

--- src/Pages/HomeSection.js
+++ src/Pages/HomeSection.js
@@ -1,17 +1,19 @@
-import { SelectedPages } from '@/shared/typesEnum'
-import React from 'react'
+import { SelectedPages } from "@/shared/typesEnum";
+import React from "react";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 function HomeSection({ setSelectedPage }) {
-    return (
-        <section id="home" className='flex items-center justify-center w-full h-screen bg-themePrimary'>
-            <motion.div
-                onViewportEnter={() => setSelectedPage(SelectedPages.Home)}>
-                Home
-            </motion.div>
-        </section>
-    )
+  return (
+    <section
+      id="home"
+      className="flex items-center justify-center w-full h-screen bg-themePrimary"
+    >
+      <motion.div onViewportEnter={() => setSelectedPage(SelectedPages.Home)}>
+        Home
+      </motion.div>
+    </section>
+  );
 }
 
-
-export default HomeSection
+export default HomeSection;
---
+++ src/app/register/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+
+    return (
+        <div>
+            <h1>Welcome to LetMeHack!</h1>
+            <p>We're here to help you create a unique, engaging, and immersive hackathon experience.</p>
+        </div>
+    );
+}
